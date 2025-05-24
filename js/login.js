cea2e7a905c9e2c1d9f814adf9b2fd61d6487505
nixon verjel <nyverjelb@ufpso.edu.co>
2025-05-24 08:52:00 -0500

    se hicieron cambios

--- js/login.js
+++ js/login.js
@@ -1,13 +1,33 @@
-document.getElementById('login-form').addEventListener('submit', function(e) {
+document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  const email = document.getElementById('email').value.trim();
-  const password = document.getElementById('password').value.trim();
 
-  if (!email || !password) {
-    alert("Todos los campos son obligatorios.");
-  } else if (!email.includes("@")) {
-    alert("Correo inválido.");
-  } else {
-    window.location.href = "index.html";
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (!username || !password) {
+    alert("Por favor, completa todos los campos.");
+    return;
   }
+
+  fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+    .then(res => {
+      if (!res.ok) throw new Error("Credenciales incorrectas");
+      return res.json();
+    })
+    .then(data => {
+      alert("Inicio de sesión exitoso");
+      window.location.href = "users.html"; 
+    })
+    .catch(error => {
+      alert("Error al iniciar sesión: " + error.message);
+    });
 });
